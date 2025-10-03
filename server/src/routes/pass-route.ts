abe4170f3e5e7368eb96a8b65a1bc43282646883
ryrahul <caulirahul6@gmail.com>
2025-10-04 05:23:23 +0530

    feat: pass schema

--- server/src/routes/pass-route.ts
+++ server/src/routes/pass-route.ts
@@ -1,5 +1,5 @@
+// src/routes/pass-route.ts
 import { Router } from "express";
-
 import { authenticate } from "../middleware/auth-middleware";
 import {
   buyPass,
@@ -7,14 +7,18 @@ import {
   getPass,
   updatePass,
 } from "../controllers/pass-controller";
+import { validateData } from "../middleware/validation-middleware";
+import { createPassSchema, updatePassSchema, buyPassSchema } from "../zod/pass-schema";
+
 
 const passRouter = Router();
 
-passRouter.post("/", authenticate, createPass);
-passRouter.post("/buy", authenticate, buyPass);
+passRouter.post("/", authenticate, validateData(createPassSchema), createPass);
+
+passRouter.put("/", authenticate, validateData(updatePassSchema), updatePass);
+
+passRouter.post("/buy", authenticate, validateData(buyPassSchema), buyPass);
 
 passRouter.get("/:creatorId", authenticate, getPass);
-
-passRouter.put("/", authenticate, updatePass);
 
 export default passRouter;
---
+++ server/src/zod/pass-schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const createPassSchema = z.object({
+  tokenMint: z.string().min(1, "tokenMint is required"),
+  price: z.number().positive("Price must be a positive number"),
+  vault_address: z.string().min(1, "vault_address is required").optional(),
+});
+
+export const updatePassSchema = z.object({
+  price: z.number().positive("Price must be a positive number"),
+});
+
+export const buyPassSchema = z.object({
+  passId: z.number().int().positive("passId must be a valid integer"),
+  txId: z.string().min(1, "Transaction ID is required"),
+});
